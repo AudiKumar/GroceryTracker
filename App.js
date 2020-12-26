b74e966e5882f7b1dc261769b2b01f06ca6e37f4
Aditya Kumar <adi6@umbc.edu>
2020-12-26 10:30:02 -0500

    setup for new mac

--- App.js
+++ App.js
@@ -2,6 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// setup when you clone the repo from github 
+// 1) clear the catche: expo r -c
+//     a) if expo cli isn't installed you should do that: npm install --global expo-cli
+// 2) npm install
+// 3) after you install all of the expo dependencies you'll be able to start the project normally: expo install
+
+
 export default function App() {
   return (
     <View style={styles.container}>
