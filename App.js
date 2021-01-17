38c4b454c128fc0b4fdcf36a35894fcd2dac0c7e
Aditya Kumar <adi6@umbc.edu>
2021-01-17 17:15:11 -0500

    got rid of the unhandled promise rejections

--- App.js
+++ App.js
@@ -12,10 +12,10 @@ import { StyleSheet, Text, View } from 'react-native';
 import {BarCodeScanner} from 'expo-barcode-scanner';
 
 //basic amplify setup
-//import { withAuthenticator } from 'aws-amplify-react-native';
+import { withAuthenticator } from 'aws-amplify-react-native';
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
-Amplify.configure(config);
+Amplify.configure({config, Analytics:{disabled: true} });
 
 //authtication 
 import {Auth} from 'aws-amplify';
@@ -55,48 +55,6 @@ const Stack = createStackNavigator();
 
 class App extends React.Component{
 
-  //all of the stuff needed for auth
-  state = {
-    username: '', 
-    password: '', 
-    phoneNumber: '', 
-    email: '', 
-    authCode: '',
-    user: {}
-  }
-
-  async signUp(){
-    const { username, password, email, phoneNumber} = this.state
-    await Auth.signUp({
-      username, 
-      password, 
-      attributes: {email, phoneNumber}
-    })
-
-    console.log('sign up successful')
-  }
-
-  async confirmSignUp(){
-    const {username, authCode} = this.state
-    await Auth.configSignignUp(username, authCode)
-    console.log('sign up has been confirmed')
-  }
-
-  async signIn(){
-    const {username, password} = this.state
-    const user = await Auth.signIn(username, password)
-
-    this.setState({ user })
-    console.log('sign in successful')
-
-  } 
-
-  async confirmSignIn(){
-    const {user, authCode} = this.state
-    await Auth.configSignignIn(user, authCode)
-    console.log('user now successfully signed in to the app!!')
-  }
-  
   render(){
     return ( 
       <NavigationContainer>
@@ -110,7 +68,8 @@ class App extends React.Component{
   }
 }
 
-//export default withAuthenticator(App, { includeGreetings: true } )
+export default withAuthenticator(App, { 
+} )
 
 const styles = StyleSheet.create({
   container: {
